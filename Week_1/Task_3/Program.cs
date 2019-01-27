26a255ba922a235723bb4c67b3204fd17a455ba9
MACBOOK <macbook@MacBook-Pro-MACBOOK.local>
2019-01-27 14:19:45 +0600

    new file

---
+++ Week_1/Task_3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            int n = int.Parse(s);
+            string t = Console.ReadLine();
+            string[] nums = t.Split();
+
+            int[] arr = new int[n];
+            int[] arr2 = new int[n];
+
+            for(int i=0; i < nums.Length; ++i)
+            {
+                arr[i] = int.Parse(nums[i]);
+                arr2[i] = int.Parse(nums[i]);
+            }
+
+            for(int i=0; i < arr.Length; ++i)
+            {
+                Console.Write(arr[i] + " " + arr2[i] + " ");
+            }
+        }
+    }
+}
