4eecdffc4310363e4b0c5eb191d606b92cdd2fc8
MACBOOK <macbook@MacBook-Pro-MACBOOK.local>
2019-01-27 15:35:52 +0600

    upd

--- Week_1/Task_4/Program.cs
+++ Week_1/Task_4/Program.cs
@@ -6,7 +6,17 @@ namespace Task_4
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string s = Console.ReadLine();
+            int n = int.Parse(s);
+
+            for(uint i=1; i <= n; ++i)
+            {
+                for(uint j=1; j <= i; ++j)
+                {
+                    Console.Write("[*]");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
