dd07aee94faac2e442b719279631a1171f0f4576
MACBOOK <macbook@MacBook-Pro-MACBOOK.local>
2019-01-28 21:56:40 +0600

    Task_2

--- Week_1/Task_1/Program.cs
+++ Week_1/Task_1/Program.cs
@@ -26,7 +26,7 @@ namespace Task_1
 
             for(int i=0; i < nums.Length; ++i)
             {
-                arr[i] = int.Parse(nums[i]);//изменить массив
+                arr[i] = int.Parse(nums[i]);
             }
 
             for(int i=0; i < arr.Length; ++i)
---
+++ Week_1/Task_2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task_2
+{
+    class MainClass
+    {
+        class Student
+        {
+            public string Name, ID;
+            public int year;
+
+            public Student()
+            {
+            }
+
+            public Student(string n, string id)
+            {
+                Name = n;
+                ID = id;
+            }
+
+            public void Print()
+            {
+                Console.WriteLine(Name + " " + ID + " " + (++year));
+            }
+        };
+        public static void Main(string[] args)
+        {
+            Student s = new Student("Assylzhan", "18BD")
+            {
+                year = Convert.ToInt32(Console.ReadLine())
+            };
+
+            s.Print();
+        }
+    }
+}
