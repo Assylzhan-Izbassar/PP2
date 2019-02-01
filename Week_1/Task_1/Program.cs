317d97f17cf358247da338af5e7a45980c004b5d
MACBOOK <macbook@MacBook-Pro-MACBOOK.local>
2019-02-01 23:17:12 +0600

    upd Task_1

--- Week_1/Task_1/Program.cs
+++ Week_1/Task_1/Program.cs
@@ -8,11 +8,11 @@ namespace Task_1
         {
             if (a < 2) return false;
             if (a == 2) return true;
-            for(uint i = 2; i*i <= a; ++i)
+            for(uint i = 2; i <= Math.Sqrt(a); ++i)//by checking prime number, if number is not divisible to the prime numbers before sqrt(a) then the number is prime
             {
-                if (a % i == 0) return false;
+                if (a % i == 0) return false;//if our element is divided to i, then it is not prime number and we return false;
             }
-            return true;
+            return true;//if element a go out from the loop, it is provide that element a is prime number, and return true;
         }
         public static void Main(string[] args)
         {
@@ -22,11 +22,11 @@ namespace Task_1
             string[] nums = t.Split();/*Split is polymorphic method that continue chars like ',', '.' , etc. and space.
                                         So in our case method is continue just spaces between elements of string and nums become new string that is not contain spaces*/
 
-            int[] arr = new int[n];
+            int[] arr = new int[n];//Create new int array arr 
 
-            for(int i=0; i < nums.Length; ++i)
+            for(int i=0; i < nums.Length; ++i)//loop for array string nums 
             {
-                arr[i] = int.Parse(nums[i]);
+                arr[i] = int.Parse(nums[i]);//each element of the string of array nums create to type int and push it like element of array arr
             }
 
             for(int i=0; i < arr.Length; ++i)
