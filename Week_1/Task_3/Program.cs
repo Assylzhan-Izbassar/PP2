50701622c13a159e7947578672362a9eb7f92959
MACBOOK <macbook@MacBook-Pro-MACBOOK.local>
2019-02-08 20:40:36 +0600

    upd

--- Week_1/Task_3/Program.cs
+++ Week_1/Task_3/Program.cs
@@ -4,33 +4,33 @@ namespace Task_3
 {
     class MainClass
     {
-        public static int[] DoubleArray(int[] arr)
+        public static int[] DoubleArray(int[] arr)//Метод принимающий массив и удваивает его каждый элемент и пишет на другой массив.
         {
-            int[] a = new int[2 * arr.Length];
+            int[] res = new int[2 * arr.Length];//Создаем новый массив res, у которого длина 2 раза больше чем длина нынешнего массива.
             for(int i=0; i < arr.Length; ++i)
             {
-                a[2*i] = a[2*i + 1] = arr[i];
+                res[2*i] = res[2*i + 1] = arr[i];//Приравневаем i элементам массива res, значение i-го элемента из массива arr.
             }
-            return a;
+            return res;//Возвращаем массив res.
         }
         public static void Main(string[] args)
         {
-            string s = Console.ReadLine();
-            int n = int.Parse(s);//метод Parse() превращает из цифра символа в цифру числа
-            string t = Console.ReadLine();
-            string[] nums = t.Split();
+            string s = Console.ReadLine();//Вводим строку s.
+            int n = int.Parse(s);//метод Parse() превращает из цифра символа в цифру числа.
+            string t = Console.ReadLine();//Вводим строку t.
+            string[] nums = t.Split();//Убираем пробелы из строки t.
 
-            int[] arr = new int[n];
+            int[] arr = new int[n];//Создаем новый массив из n элементов.
 
-            for (int i = 0; i < nums.Length; ++i)
+            for (int i = 0; i < nums.Length; ++i)//Пробегаемся по массиву nums.
             {
-                arr[i] = int.Parse(nums[i]);
+                arr[i] = int.Parse(nums[i]);//Превращаем i-ты элемент из nums в integer и преравневаем его значение на i место массива arr.
             }
-            int[] arr2 = DoubleArray(arr);
+            int[] arr2 = DoubleArray(arr);//Вызываем метод DoubleArray чтобы дать значение массиву arr2. 
 
-            for (int i = 0; i < arr2.Length; ++i)
+            for (int i = 0; i < arr2.Length; ++i)//Пробегаемся  по массиву arr2.
             {
-                Console.Write(arr2[i] + " ");
+                Console.Write(arr2[i] + " ");//Выводим элементы arr2.
             }
         }
     }
