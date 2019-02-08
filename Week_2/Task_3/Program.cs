902748fa9df5a39084afbe7981027364d403af39
MACBOOK <macbook@MacBook-Pro-MACBOOK.local>
2019-02-08 21:04:18 +0600

    Task_3

---
+++ Week_2/Task_3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Task_3
+{
+    class MainClass
+    {
+        public static void PrintInfo(FileSystemInfo fs, int n)
+        {
+            string line = new string(' ', n);
+            line = line + fs.Name;
+            Console.WriteLine(line);
+
+            if(fs is DirectoryInfo)
+            {
+                var items = (fs as DirectoryInfo).GetFileSystemInfos();
+
+                foreach(var i in items)
+                {
+                    PrintInfo(i, n + 4);
+                }
+
+            }
+        }
+        public static void Main(string[] args)
+        {
+            string path = @"/Users/macbook/Desktop/PP2/Projects/PP2/Week_1/Task_1";
+            DirectoryInfo dir = new DirectoryInfo(path);
+            PrintInfo(dir, 0);
+        }
+    }
+}
