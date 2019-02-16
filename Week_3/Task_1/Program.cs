5b6046c1535f0e908f7930ab0d92db233f0ee778
MACBOOK <macbook@MacBook-Pro-MACBOOK.local>
2019-02-16 12:53:27 +0600

    upd

--- Week_3/Task_1/Program.cs
+++ Week_3/Task_1/Program.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task_1
 {       
@@ -149,10 +146,6 @@ namespace Task_1
                         break;
                 }
             }
-            
-
-            //Console.ReadKey();
-
         }
     }
 }
